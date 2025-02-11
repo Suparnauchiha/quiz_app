a522848438eae24aa54b616d5ee030bbb699a81a
Suparnauchiha <suparnaurmipaul2004@gmail.com>
2025-02-11 21:41:32 +0530

    edit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 router.get("/", getAllUsers);           // Get all users
 router.get("/:id", getUserById);        // Get a user by ID
 router.post("/", createUser);           // Create a new user
-router.patch("/:id", updateUser);      // Update user details
+router.put("/:id", updateUser);      // Update user details
 router.delete("/:id", deleteUser);     // Delete a user
 
 module.exports = router;
